1a99079c921d61c8328b246219fb30bd6ffe682b
Afiz Savage <savageafiz9@gmail.com>
2022-05-03 15:35:32 +0000

    Created database tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_503_142_945) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_142945) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'categories', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.string 'name'
-    t.string 'icon'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_categories_on_user_id'
+  create_table "categories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table 'transactions', force: :cascade do |t|
-    t.bigint 'author_id', null: false
-    t.bigint 'category_id', null: false
-    t.string 'name'
-    t.decimal 'amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_transactions_on_author_id'
-    t.index ['category_id'], name: 'index_transactions_on_category_id'
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "category_id", null: false
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_transactions_on_author_id"
+    t.index ["category_id"], name: "index_transactions_on_category_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'categories', 'users'
-  add_foreign_key 'transactions', 'categories'
-  add_foreign_key 'transactions', 'users', column: 'author_id'
+  add_foreign_key "categories", "users"
+  add_foreign_key "transactions", "categories"
+  add_foreign_key "transactions", "users", column: "author_id"
 end
